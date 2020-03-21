3c1325e64f611ad30209dbca2d433e8878c00c89
torvarun <torvarun@gmail.com>
2020-03-20 23:34:23 -0400

    polybar on multiple screens

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,4 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -r torvarun &
+# polybar -r torvarun &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload torvarun &
+  done
+else
+  polybar --reload torvarun &
+fi
